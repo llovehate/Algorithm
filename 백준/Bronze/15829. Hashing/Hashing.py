dce8bf508dbd3784137988e100cffff04b0ef45c
Yejin Park <eksk9044@naver.com>
2025-03-09 23:36:25 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,40 +1,15 @@
 N = int(input())
 my_str = input()
 
-alpha_dict = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4,
-    'e': 5,
-    'f': 6,
-    'g': 7,
-    'h': 8,
-    'i': 9,
-    'j': 10,
-    'k': 11,
-    'l': 12,
-    'm': 13,
-    'n': 14,
-    'o': 15,
-    'p': 16,
-    'q': 17,
-    'r': 18,
-    's': 19,
-    't': 20,
-    'u': 21,
-    'v': 22,
-    'w': 23,
-    'x': 24,
-    'y': 25,
-    'z': 26
-}
+r = 31
+M = 1234567891
 
 result = 0
-start = 0
+power = 1
 
-for i in my_str:
-    result += alpha_dict[i] * (31 ** start)
-    start += 1
+for i in range(N):
+    str_v = ord(my_str[i]) - ord('a') + 1
+    result = (result + str_v * power) % M
+    power = (power * r) % M
 
 print(result)
